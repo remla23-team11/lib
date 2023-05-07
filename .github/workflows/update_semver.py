fe6d652547327e1752330cae9336d4491cfc26f9
Junbo Xiong <19368807+C6H5-NO2@users.noreply.github.com>
2023-05-07 16:06:48 +0200

    fix: semver update file open mode

--- .github/workflows/update_semver.py
+++ .github/workflows/update_semver.py
@@ -4,12 +4,13 @@ import sys
 
 def main():
     semver = sys.argv[1]
-    with open('./package.json', 'wt', encoding='utf-8') as f:
+    with open('./package.json', 'rt', encoding='utf-8') as f:
         j = json.load(f)
-        oldver = j['version']
-        newver = semver[1:]
-        print(f'from {oldver} to {newver}')
-        j['version'] = newver
+    oldver = j['version']
+    newver = semver[1:]
+    print(f'from {oldver} to {newver}')
+    j['version'] = newver
+    with open('./package.json', 'wt', encoding='utf-8') as f:
         json.dump(j, f, ensure_ascii=False, indent=4)
 
 
